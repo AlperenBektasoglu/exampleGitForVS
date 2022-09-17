02dc7868f5bf59f95d89fc9d4aa36f1e50483000
Alperen Bektasoglu <alperenbektasoglu.info@gmail.com>
2022-09-17 19:09:06 +0300

    deneme created

---
+++ deneme.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exampleGitForVS
+{
+    internal class deneme
+    {
+        int x = 0;
+    }
+}
